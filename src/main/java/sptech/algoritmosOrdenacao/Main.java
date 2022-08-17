1ddb1a32a5763b6337d890f1d613e33d52a1e6a5
EnzoGodoy21 <enzo.godoy@bandtec.com.br>
2022-08-17 15:01:01 -0300

    Adicionando pesquisa binária

--- src/main/java/sptech/algoritmosOrdenacao/Main.java
+++ src/main/java/sptech/algoritmosOrdenacao/Main.java
@@ -1,5 +1,7 @@
 package sptech.algoritmosOrdenacao;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void showArray(int[] array, int i) {
@@ -20,38 +22,44 @@ public class Main {
     public static void insertSort(int[] v) {
         int aux = 0;
         for (int i = 0; i < v.length; i++) {
-            int j = i-1;
+            int j = i - 1;
             aux = v[i];
-            while(j>=0 && v[j]>aux) v[j+1] = v[j--];
-            v[j+1] = aux;
+            while (j >= 0 && v[j] > aux)
+                v[j + 1] = v[j--];
+            v[j + 1] = aux;
         }
     }
 
     public static void mergeSort(int[] v, int start, int end) {
-        if(start < end-1){
+        if (start < end - 1) {
             int middle = (start + end) / 2;
-            mergeSort(v,start,middle);
-            mergeSort(v,middle,end);
-            merge(v,start,middle,end);
+            mergeSort(v, start, middle);
+            mergeSort(v, middle, end);
+            merge(v, start, middle, end);
         }
     }
 
-    public static void merge(int[] v, int start, int middle, int end){
+    public static void merge(int[] v, int start, int middle, int end) {
         int i = start;
         int j = middle;
         int k = 0;
-        int[] w = new int[end-start];
-        while(i<middle && j< end) {
-            if(v[i] <= v[j]) w[k++] = v[i++];
-            else w[k++] = v[j++];
+        int[] w = new int[end - start];
+        while (i < middle && j < end) {
+            if (v[i] <= v[j])
+                w[k++] = v[i++];
+            else
+                w[k++] = v[j++];
         }
-        while(i<middle) w[k++] = v[i++];
-        while(j<end) w[k++] = v[j++];
-        for(int q = start; q <= end-1; q++) v[q] = w[q-start];
+        while (i < middle)
+            w[k++] = v[i++];
+        while (j < end)
+            w[k++] = v[j++];
+        for (int q = start; q <= end - 1; q++)
+            v[q] = w[q - start];
     }
 
-    public static void quickSort(int[] v){
-        partition(v,0,v.length-1);
+    public static void quickSort(int[] v) {
+        partition(v, 0, v.length - 1);
     }
 
     public static void partition(int v[], int indInicio, int indFim){
@@ -73,4 +81,35 @@ public class Main {
         if(i < indFim) partition(v,i,indFim);
     }
 
+    public static void pesquisaBinariaIterativa(int[] vetor, int numProcurado){
+        int i = 0, j = 0;while(i < vetor.length){
+            if(vetor[i] == numProcurado){
+                System.out.println("Número " + numProcurado + "encontrado após " + i + "iterações");
+                return;
+            }
+            i++;
+        }
+        System.out.println("Numero " + numProcurado + "não encontrado mesmo após " + i + "iterações");
+    }
+    
+    public static void pesquisabinariaRecursiva(int[] vetor, int numProcurado, int qtdIteracoes){
+        if(vetor.length == 1 && vetor[0]==numProcurado){
+            System.out.println("Numero " + numProcurado + "não encontrado mesmo após " + qtdIteracoes + "iterações");
+            return;
+        }
+        int meio = vetor[vetor.length/2];
+        
+        if(meio == numProcurado){
+            System.out.println("Número " + numProcurado + "encontrado após " + qtdIteracoes + "iterações");
+        }
+
+        if(meio > numProcurado){
+            pesquisabinariaRecursiva(Arrays.copyOfRange(vetor, 0, vetor.length/2),numProcurado,++qtdIteracoes);
+        }else{
+            pesquisabinariaRecursiva(Arrays.copyOfRange(vetor,vetor.length/2,vetor.length),numProcurado, ++qtdIteracoes);
+        }
+    }
+
 }
+
+    
